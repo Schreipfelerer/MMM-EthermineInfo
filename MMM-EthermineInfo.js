d95bb0612fdf3e82cfb5395376f1fcbf0432fbbd
Schreipfelerer <44339978+Schreipfelerer@users.noreply.github.com>
2021-05-14 15:21:34 +0200

    First Commit

---
+++ MMM-EthermineInfo.js
@@ -0,0 +1,143 @@
+Module.register("MMM-EthermineInfo",{
+// ##################################################################################
+// Defines config default values
+// ##################################################################################
+    defaults: {
+        minerAdress: undefined,
+        updateInterval: 1000,
+        fetchIntervall: 1200000,
+        showHeader: true,
+        headerText: "Ethermine Stats for %miner",
+        fontSize: 'xx-large',
+
+        initialLoadDelay: 0,
+
+        apiBase: "https://api.ethermine.org",
+    },
+
+// ##################################################################################
+// import css files
+// ##################################################################################
+	getStyles: function() {
+		return ["MMM-EthermineInfo.css"]
+	},
+
+// ##################################################################################
+// setup the module
+// ##################################################################################
+	start: function() {
+		console.info('Starting module: ' + this.name)
+		this.loaded = false
+        this.updateHashrate()
+
+
+		this.scheduleUpdate()
+		
+		var self = this
+		setInterval(function() {
+			self.updateDom()
+		}, this.config.updateInterval)
+
+	},
+    
+// ##################################################################################
+// handle all the output stuff
+// ##################################################################################
+    getDom: function() {
+        var wrapper = document.createElement("div")
+
+        if(this.debug != undefined){
+            wrapper.innerHTML = this.debug
+			wrapper.className = "loading-wrapper"
+			return wrapper
+        }
+
+        if (!this.loaded) {
+			wrapper.innerHTML = this.translate('LOADING')
+			wrapper.className = "loading-wrapper"
+			return wrapper
+		}
+
+        if (this.config.minerAdress == undefined){
+            wrapper.innerHTML = "You need to add your minerAdress in config.js"
+			wrapper.className = "dimmed light small"
+			return wrapper
+        }
+
+        if (this.currentHashrate == undefined) {
+            wrapper.innerHTML = "no data"
+			wrapper.className = "dimmed light small"
+			return wrapper
+		}
+
+        if(this.config.showHeader) {
+            var header = document.createElement("header")
+            header.className = "module-header"
+            header.innerHTML = this.config.headerText.replace("%miner", this.config.minerAdress.toString())
+            wrapper.appendChild(header)
+        }
+
+        var datawrapper = document.createElement("div")
+
+        var currentWraper = document.createElement("div")
+        currentWraper.innerHTML = "Current Hashrate: " + (this.currentHashrate/Math.pow(10, 6)).toFixed(2) + "MH/s"
+        currentWraper.className = "Current_Hashrate_Wraper"
+        currentWraper.style.fontSize = this.config.fontSize
+        datawrapper.appendChild(currentWraper)
+
+        var reportedWraper = document.createElement("div")
+        reportedWraper.innerHTML = "Reported Hashrate: " + (this.reportedHashrate/Math.pow(10, 6)).toFixed(2) + "MH/s"
+        reportedWraper.className = "Reported_Hashrate_Wraper"
+        reportedWraper.style.fontSize = this.config.fontSize
+        datawrapper.appendChild(reportedWraper)
+
+        wrapper.appendChild(datawrapper)
+        return wrapper
+    },
+
+// ##################################################################################
+// fetch new data
+// ##################################################################################
+    updateHashrate: function() {
+        if (this.config.minerAdress == undefined){
+            return
+        }
+        var url = this.config.apiBase + "/miner/" + this.config.minerAdress + "/dashboard"
+        var Request = new XMLHttpRequest()
+		Request.open("GET", url, true)
+        
+        var self = this
+        Request.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					self.processHashrate(JSON.parse(this.response))
+				} else {
+					console.error(self.name + ": Could not load data.")
+				}
+			}
+		}
+        Request.send()
+    },
+
+// ##################################################################################
+// validate data after request, prepare for output
+// ##################################################################################
+    processHashrate: function(data) {
+        this.currentHashrate = data.data.currentStatistics["currentHashrate"]
+        this.reportedHashrate = Math.floor(data.data.currentStatistics["reportedHashrate"])
+
+        this.loaded = true
+        this.updateDom()
+    },
+
+// ##################################################################################
+// schedule updates based on define interval
+// ##################################################################################
+    scheduleUpdate: function(delay) {
+		var nextLoad = this.config.fetchIntervall
+		var self = this
+		setInterval(function() {
+			self.updateHashrate()
+		}, nextLoad)
+	},
+})
---
+++ README.md
@@ -0,0 +1,69 @@
+# MMM-Ethermine_Info
+A <a href="https://github.com/MichMich/MagicMirror">MagicMirror</a> module that shows your current Ethermine stats.
+
+## Installation
+1. Navigate into your MagicMirror's `modules` folder using `cd MagicMirror/modules/`
+2. Clone the module using `git clone https://github.com/Schreipfelerer/MMM-Ethermine_Info.git`.
+3. Add the module inside `config.js`
+
+## Config
+
+|Option|Description|
+|---|---|
+|`minerAdress`|MANDATORY: The Etherium adress of the wallet the miner is mining to.<br>**Type:** `string`|
+|`updateInterval`|Frequency, in milliseconds, to update the module<br>**Type:** `number` <br> **Default** <i>1000</i>|
+|`fetchIntervall`|Frequency, in milliseconds, to fetch new data.<br>**Type:** `number` <br> **Default** <i>1200000</i>|
+|`showHeader`|Specefies wether a header should be shown or not.<br>**Type:** `boolean`<br> **Default** <i>true</i> |
+|`headerText`|The Text which is shown in the header. You can use `%miner` to reference your minerAdress.<br> **Type** `string` <br> **Default** <i>Ethermine Stats for %miner</i> |
+|`fontSize`| Dimension of price text. You can specify pixel values, em values or keywords.<br> **Type:** `string` <br>**Options:** `xx-small`, `x-small`, `small`, `medium`, `large`, `x-large`, `xx-large` <br> **Default** <i>xx-large</i> |
+
+
+Here is an example of an entry in `config.js`
+```
+{
+	module: "MMM-Ethermine_Info",
+	position: "bottom_right",
+	config: {
+		minerAdress: "YOUR ADRESS",
+        fontSize: "large"
+	}
+}
+```
+
+## Screenshot
+![Screenshot of working module](/Info.png?raw=true "Example screenshot")
+
+## Notes
+This module uses the Ethermine <a href="https://ethermine.org/api/pool">API</a>.
+- The API does not require a Key
+- Endpoints update every 2 minutes.
+- The API is limited to 100 requests / 15 minutes / IP
+
+## Feedback
+It's my first module here after that I built a MagicMirror. It also is the first Time i used javascript in gerneral.
+<br>Since i dindt know where to start i used the <a href="https://github.com/matteodanelli/MMM-cryptocurrency">MMM-cryptocurrency</a> module as a guideline.
+<br>I am open to work on this project and to expand it to add other interesting features, and a bit of cool style too.
+<br>Leave me some feedback in the forum. Thank you!
+
+
+## License
+The MIT License (MIT)
+=====================
+
+Copyright © 2021 Schreipfelerer
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the “Software”), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+**The software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability,
+fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability,
+whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.**
